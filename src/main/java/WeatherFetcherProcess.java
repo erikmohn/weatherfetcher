1b061531f9626472471be192d5b38eaa9d1cff56
Erik Mohn <mohn.erik@gmail.com>
2013-06-24 21:10:39 +0200

    Added missing code to execute fetching of data

--- src/main/java/WeatherFetcherProcess.java
+++ src/main/java/WeatherFetcherProcess.java
@@ -1,7 +1,11 @@
 public class WeatherFetcherProcess {
 
 	public static void main(String[] args) {
-			
+			try {
+				new WeatherFetcher().execute();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 
 }
