eec1ccc32ca789af6fb47ecb77074e71c3e5bbee
Erik Mohn <mohn.erik@gmail.com>
2014-06-12 20:45:34 +0200

    Fixed wrong vindsidenId for Holfuy stations

--- src/main/java/no/vindsiden/parser/impl/support/holfuy/HolfuyStationId.java
+++ src/main/java/no/vindsiden/parser/impl/support/holfuy/HolfuyStationId.java
@@ -2,11 +2,11 @@ package no.vindsiden.parser.impl.support.holfuy;
 
 public enum HolfuyStationId {
 	s119(2119), 
-	s124(2024),
+	s124(2124),
 	s108(2108),
 	s112(2112),
 	s118(2118),
-	s107(2117);
+	s107(2107);
 	
 	private Integer vindsidenId;
 	
